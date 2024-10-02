6eb0d134b80a7e3084a349f7284a271f1f5239e8
Pavel Kocman <kocmanpavel@gmail.com>
2024-10-02 20:35:56 +0200

    fix: Fix web build script

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TrpcRouter } from '@server/trpc/trpc.router';
 
+const PORT = process.env.PORT || 4000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const trpc = app.get(TrpcRouter);
   trpc.applyMiddleware(app);
-  await app.listen(process.env.PORT || 4000);
+  await app.listen(PORT, () =>
+    console.log('Server is listening on port ', PORT),
+  );
 }
 bootstrap();
